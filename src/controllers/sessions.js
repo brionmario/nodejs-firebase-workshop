958c812b1a6c2ce105327096ba0c2b7731069514
DonOmalVindula <omalvindula@live.com>
2023-07-24 13:28:31 +0530

    Fix formatting

--- src/controllers/sessions.js
+++ src/controllers/sessions.js
@@ -1,4 +1,6 @@
-const { db } = require("../firebase/admin");
+const { db } = require('../firebase/admin');
+
+const SESSION_COLLECTION = 'sessions';
 
 /**
  * Get all the sessions.
@@ -6,23 +8,24 @@ const { db } = require("../firebase/admin");
  * @param {*} res - Response object.
  */
 const getSessions = (req, res) => {
-    const sessionRef = db.collection("sessions");
+  const sessionRef = db.collection(SESSION_COLLECTION);
 
-    sessionRef
-        .get()
-        .then((snapshot) => {
-            const data = snapshot.docs.map((doc) => ({
-                id: doc.id,
-                ...doc.data(),
-            }));
-            return res.status(201).json(data);
-        })
-        .catch((error) => {
-            return res.status(500).json({
-                message: "Error while getting sessions.",
-                error: error,
-            });
-        });
+  sessionRef
+    .get()
+    .then((snapshot) => {
+      const data = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+
+      return res.status(201).json(data);
+    })
+    .catch((error) => {
+      return res.status(500).json({
+        message: 'Error while getting the sessions.',
+        error: error,
+      });
+    });
 };
 
 /**
@@ -31,31 +34,31 @@ const getSessions = (req, res) => {
  * @param {*} res - Response object.
  */
 const getSession = (req, res) => {
-    const id = parseInt(req.params.id, 10);
-    const sessionRef = db.collection("sessions").where("id", "==", id);
+  const id = parseInt(req.params.id, 10);
+  const sessionRef = db.collection(SESSION_COLLECTION).where('id', '==', id);
 
-    sessionRef
-        .get()
-        .then((snapshot) => {
-            const data = snapshot.docs.map((doc) => ({
-                id: doc.id,
-                ...doc.data(),
-            }));
+  sessionRef
+    .get()
+    .then((snapshot) => {
+      const data = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
 
-            if (data.length === 0) {
-                return res.status(404).json({
-                    message: `No session found for the given session id: ${id}.`,
-                });
-            }
-
-            return res.status(201).json(data);
-        })
-        .catch((error) => {
-            return res.status(500).json({
-                message: "Error while getting session.",
-                error: error,
-            });
+      if (data.length === 0) {
+        return res.status(404).json({
+          message: `No session found for the given session id: ${id}.`,
         });
+      }
+
+      return res.status(201).json(data);
+    })
+    .catch((error) => {
+      return res.status(500).json({
+        message: 'Error while getting the session.',
+        error: error,
+      });
+    });
 };
 
 /**
@@ -64,30 +67,30 @@ const getSession = (req, res) => {
  * @param {*} res - Response object.
  */
 const createSession = async (req, res) => {
-    const sessionRef = db.collection("sessions");
-    const snapshot = await sessionRef.count().get();
-    const docId = snapshot.data().count + 1;
+  const sessionRef = db.collection(SESSION_COLLECTION);
+  const snapshot = await sessionRef.count().get();
+  const docId = snapshot.data().count + 1;
 
-    const session = {
-        id: docId,
-        ...req.body,
-    };
+  const session = {
+    id: docId,
+    ...req.body,
+  };
 
-    sessionRef
-        .doc(docId.toString())
-        .set(session)
-        .then((docRef) => {
-            return res.status(201).json({
-                id: docRef.id,
-                ...session,
-            });
-        })
-        .catch((error) => {
-            return res.status(500).json({
-                message: "Error while adding session.",
-                error: error,
-            });
-        });
+  sessionRef
+    .doc(docId.toString())
+    .set(session)
+    .then((docRef) => {
+      return res.status(201).json({
+        id: docRef.id,
+        ...session,
+      });
+    })
+    .catch((error) => {
+      return res.status(500).json({
+        message: 'Error while adding the session.',
+        error: error,
+      });
+    });
 };
 
 /**
@@ -96,41 +99,41 @@ const createSession = async (req, res) => {
  * @param {*} res - Response object.
  */
 const updateSession = (req, res) => {
-    const id = parseInt(req.params.id, 10);
-    const updatedSession = req.body;
+  const id = parseInt(req.params.id, 10);
+  const updatedSession = req.body;
 
-    const sessionDocRef = db.collection("sessions").doc(id.toString());
+  const sessionDocRef = db.collection(SESSION_COLLECTION).doc(id.toString());
 
-    sessionDocRef
-        .get()
-        .then((doc) => {
-            if (doc.exists) {
-                sessionDocRef
-                    .update(updatedSession)
-                    .then(() => {
-                        return res.status(201).json({
-                            id: doc.id,
-                            ...updatedSession,
-                        });
-                    })
-                    .catch((error) => {
-                        return res.status(500).json({
-                            message: "Error while updating session.",
-                        });
-                    });
-            } else {
-                return res.status(404).json({
-                    message: `No session found for the given session id: ${id}.`,
-                });
-            }
-        })
-        .catch((error) => {
-            console.log(error);
-            return res.status(500).json({
-                message: "Error while getting the session to update.",
-                error: error,
+  sessionDocRef
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        sessionDocRef
+          .update(updatedSession)
+          .then(() => {
+            return res.status(201).json({
+              id: doc.id,
+              ...updatedSession,
             });
+          })
+          .catch((error) => {
+            return res.status(500).json({
+              message: 'Error while updating session.',
+              error: error,
+            });
+          });
+      } else {
+        return res.status(404).json({
+          message: `No session found for the given session id: ${id}.`,
         });
+      }
+    })
+    .catch((error) => {
+      return res.status(500).json({
+        message: 'Error while getting the session to update.',
+        error: error,
+      });
+    });
 };
 
 /**
@@ -139,37 +142,37 @@ const updateSession = (req, res) => {
  * @param {*} res - Response object.
  */
 const deleteSession = (req, res) => {
-    const id = parseInt(req.params.id, 10);
+  const id = parseInt(req.params.id, 10);
 
-    const sessionDocRef = db.collection("sessions").doc(id.toString());
+  const sessionDocRef = db.collection(SESSION_COLLECTION).doc(id.toString());
 
-    sessionDocRef
-        .get()
-        .then((doc) => {
-            if (doc.exists) {
-                sessionDocRef.delete().then(() => {
-                    return res.status(201).json({
-                        message: `Session with id ${id} deleted successfully.`,
-                    });
-                });
-            } else {
-                return res.status(404).json({
-                    message: `No session found for the given session id: ${id}.`,
-                });
-            }
-        })
-        .catch((error) => {
-            return res.status(500).json({
-                message: "Error while getting the session to delete.",
-                error: error,
-            });
+  sessionDocRef
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        sessionDocRef.delete().then(() => {
+          return res.status(201).json({
+              message: `Session with id ${id} deleted successfully.`,
+          });
         });
+      } else {
+        return res.status(404).json({
+            message: `No session found for the given session id: ${id}.`,
+        });
+      }
+    })
+    .catch((error) => {
+      return res.status(500).json({
+        message: 'Error while getting the session to delete.',
+        error: error,
+      });
+    });
 };
 
 module.exports = {
-    getSessions,
-    getSession,
-    createSession,
-    updateSession,
-    deleteSession,
+  getSessions,
+  getSession,
+  createSession,
+  updateSession,
+  deleteSession,
 };
--- src/firebase/admin.js
+++ src/firebase/admin.js
@@ -1,6 +1,6 @@
 
-var admin = require("firebase-admin");
-var serviceAccount = require("./private-key.json");
+var admin = require('firebase-admin');
+var serviceAccount = require('./private-key.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
